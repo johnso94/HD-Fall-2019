ecfb60b68cb1da1e816c87c34adbf7df888c95ba
johnso94@stolaf.edu <johnso94@raspberrypi>
2019-09-19 21:07:35 -0500

    lab5: PartA complete

---
+++ lab5/power.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+
+int power(int base, int exp){
+  int i = base;
+  for (int q = 1, q<exp, q++){
+    i *= base;
+  }
+  return i;
+}
+
+int main(){
+  int a;
+  int b;
+  printf("Enter two integers:\n");
+  scanf("%d",&a);
+  scanf("%d",&b);
+  printf("%d raised to %d is %d",a,b,power(a,b));
+}

54022f4789efbc285e8f15e6af15ca03892e2692
johnso94@stolaf.edu <johnso94@raspberrypi>
2019-09-22 20:18:55 -0500

    hw3: revised max.c, min.c

--- hw3/min.c
+++ hw3/min.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <math.h>
 
-double max(double x, double y) {
-  if (x<y)
+double min(double x, double y) {
+  if (x>y)
     return y;
   else
     return x;
@@ -11,6 +11,6 @@ float a, b;
 int main(){
   printf("enter two values: ");
   scanf("%f %f", &a, &b);
-  printf("The call max(%f, %f) returns the value %f\n",a,b,max(a,b));
+  printf("The call min(%f, %f) returns the value %f\n",a,b,min(a,b));
   return 0;
 }

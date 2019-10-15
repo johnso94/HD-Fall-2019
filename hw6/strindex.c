2b6098a95bc74a5b29aceb6af60486a60ac6844f
johnso94@stolaf.edu <johnso94@raspberrypi>
2019-10-15 12:43:50 -0500

    hw6:Part B2 final version - complete

--- hw6/strindex.c
+++ hw6/strindex.c
@@ -3,18 +3,20 @@
 
 int strindex(char str[], char val){
   int i = 0;
-  while(str[i] != val){
-    i++;
-    if (str[i] == '\0'){
+  char *ptr = str;
+  while(*ptr != val){
+    if (*ptr == '\0'){
       return -1;
     }
+    i++;
+    ptr ++;
   }
   return i;
 }
 
 int main(){
   int x;
-  x = strindex("The end", 'a');
+  x = strindex("The end", 'e');
   printf("x contains %d\n", x);
   return 0;
 }

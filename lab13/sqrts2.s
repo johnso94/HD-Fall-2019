5eb8f1e61fde5c4e8426c0b5c95eae227b53bb4d
johnso94@stolaf.edu <johnso94@raspberrypi>
2019-10-19 12:10:37 -0500

    lab13: Part A complete

---
+++ lab13/sqrts2.s
@@ -0,0 +1,69 @@
+	.arch armv6
+	.eabi_attribute 28, 1
+	.eabi_attribute 20, 1
+	.eabi_attribute 21, 1
+	.eabi_attribute 23, 3
+	.eabi_attribute 24, 1
+	.eabi_attribute 25, 1
+	.eabi_attribute 26, 2
+	.eabi_attribute 30, 6
+	.eabi_attribute 34, 1
+	.eabi_attribute 18, 4
+	.file	"sqrts2.c"
+	.text
+	.section	.rodata
+	.align	2
+.LC0:
+	.ascii	"n  sqrt(n)\012----------\000"
+	.align	2
+.LC1:
+	.ascii	"%d  %f\012\000"
+	.text
+	.align	2
+	.global	main
+	.arch armv6
+	.syntax unified
+	.arm
+	.fpu vfp
+	.type	main, %function
+main:
+	@ args = 0, pretend = 0, frame = 8
+	@ frame_needed = 1, uses_anonymous_args = 0
+	push	{fp, lr}
+	add	fp, sp, #4
+	sub	sp, sp, #8
+	ldr	r0, .L5
+	bl	puts
+	mov	r3, #0
+	str	r3, [fp, #-8]
+	b	.L2
+.L3:
+	ldr	r3, [fp, #-8]
+	vmov	s15, r3	@ int
+	vcvt.f64.s32	d7, s15
+	vmov.f64	d0, d7
+	bl	sqrt
+	vmov	r2, r3, d0
+	ldr	r1, [fp, #-8]
+	ldr	r0, .L5+4
+	bl	printf
+	ldr	r3, [fp, #-8]
+	add	r3, r3, #1
+	str	r3, [fp, #-8]
+.L2:
+	ldr	r3, [fp, #-8]
+	cmp	r3, #5
+	ble	.L3
+	mov	r3, #0
+	mov	r0, r3
+	sub	sp, fp, #4
+	@ sp needed
+	pop	{fp, pc}
+.L6:
+	.align	2
+.L5:
+	.word	.LC0
+	.word	.LC1
+	.size	main, .-main
+	.ident	"GCC: (Raspbian 8.3.0-6+rpi1) 8.3.0"
+	.section	.note.GNU-stack,"",%progbits

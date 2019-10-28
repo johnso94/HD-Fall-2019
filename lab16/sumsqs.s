402010c97ba38de780f64f8abe0b456948d8c1d9
johnso94@stolaf.edu <johnso94@raspberrypi>
2019-10-27 20:25:58 -0500

    lab16:Part A complete

---
+++ lab16/sumsqs.s
@@ -0,0 +1,82 @@
+	.arch armv6
+	.eabi_attribute 28, 1
+	.eabi_attribute 20, 1
+	.eabi_attribute 21, 1
+	.eabi_attribute 23, 3
+	.eabi_attribute 24, 1
+	.eabi_attribute 25, 1
+	.eabi_attribute 26, 2
+	.eabi_attribute 30, 6
+	.eabi_attribute 34, 1
+	.eabi_attribute 18, 4
+	.file	"sumsqs.c"
+	.text
+	.section	.rodata
+	.align	2
+.LC0:
+	.ascii	"Enter a positive integer:  \000"
+	.align	2
+.LC1:
+	.ascii	"%d\000"
+	.align	2
+.LC2:
+	.ascii	"The sum of the first %d squares is %d.\012\000"
+	.text
+	.align	2
+	.global	main
+	.arch armv6
+	.syntax unified
+	.arm
+	.fpu vfp
+	.type	main, %function
+main:
+	@ args = 0, pretend = 0, frame = 16
+	@ frame_needed = 1, uses_anonymous_args = 0
+	push	{fp, lr}
+	add	fp, sp, #4
+	sub	sp, sp, #16
+	mov	r3, #0
+	str	r3, [fp, #-8]
+	ldr	r0, .L5
+	bl	printf
+	sub	r3, fp, #16
+	mov	r1, r3
+	ldr	r0, .L5+4
+	bl	__isoc99_scanf
+	mov	r3, #0
+	str	r3, [fp, #-12]
+	b	.L2
+.L3:
+	ldr	r3, [fp, #-12]
+	ldr	r2, [fp, #-12]
+	mul	r3, r2, r3
+	ldr	r2, [fp, #-8]
+	add	r3, r2, r3
+	str	r3, [fp, #-8]
+	ldr	r3, [fp, #-12]
+	add	r3, r3, #1
+	str	r3, [fp, #-12]
+.L2:
+	ldr	r3, [fp, #-16]
+	ldr	r2, [fp, #-12]
+	cmp	r2, r3
+	ble	.L3
+	ldr	r3, [fp, #-16]
+	ldr	r2, [fp, #-8]
+	mov	r1, r3
+	ldr	r0, .L5+8
+	bl	printf
+	mov	r3, #0
+	mov	r0, r3
+	sub	sp, fp, #4
+	@ sp needed
+	pop	{fp, pc}
+.L6:
+	.align	2
+.L5:
+	.word	.LC0
+	.word	.LC1
+	.word	.LC2
+	.size	main, .-main
+	.ident	"GCC: (Raspbian 8.3.0-6+rpi1) 8.3.0"
+	.section	.note.GNU-stack,"",%progbits
